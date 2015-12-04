38e410730cd8aea8111e9c0b681daaf6c90aec13
Michael Feathers <michael.feathers@gmail.com>
2015-12-04 10:11:48 -0500

    added constants for on and off events

--- lib/out_port.rb
+++ lib/out_port.rb
@@ -3,23 +3,27 @@
 require 'unimidi'
 
 class OutPort
+
+  MIDI_ON = 0x90
+  MIDI_OFF = 0x80
+
   def initialize output = UniMIDI::Output.first
     @output = output
     @active_notes = {}
   end
 
   def send_on note, velocity
-    @output.puts(0x90, note, velocity)
+    @output.puts(MIDI_ON, note, velocity)
     @active_notes[note] = velocity
   end
 
   def send_off note, velocity
-    @output.puts(0x80, note, velocity)
+    @output.puts(MIDI_OFF, note, velocity)
   end
 
   def clear
     @active_notes.each do |note, velocity|
-      @output.puts(0x80, note, velocity)
+      @output.puts(MIDI_OFF, note, velocity)
     end
     @active_notes = {}
   end
--- spec/out_port_spec.rb
+++ spec/out_port_spec.rb
@@ -9,18 +9,18 @@ describe OutPort do
   end
 
   it 'sends a midi note-on event' do
-    expect(@midi_out).to receive(:puts).with(0x90, 40, 100)
+    expect(@midi_out).to receive(:puts).with(OutPort::MIDI_ON, 40, 100)
     OutPort.new(@midi_out).send_on(40, 100)
   end
 
   it 'sends a midi note-off event' do
-    expect(@midi_out).to receive(:puts).with(0x80, 40, 100)
+    expect(@midi_out).to receive(:puts).with(OutPort::MIDI_OFF, 40, 100)
     OutPort.new(@midi_out).send_off(40, 100)
   end
 
   it 'clears all playing notes' do
-    expect(@midi_out).to receive(:puts).with(0x90, 40, 100)
-    expect(@midi_out).to receive(:puts).with(0x80, 40, 100)
+    expect(@midi_out).to receive(:puts).with(OutPort::MIDI_ON, 40, 100)
+    expect(@midi_out).to receive(:puts).with(OutPort::MIDI_OFF, 40, 100)
     out = OutPort.new(@midi_out)
     out.send_on(40,100)
     out.clear
