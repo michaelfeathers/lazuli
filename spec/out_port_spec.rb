510477d1fd7ab3e34c06a8608a8773bbb03fcd57
Michael Feathers <michael.feathers@gmail.com>
2015-12-04 10:38:56 -0500

    spec cleanup

--- spec/out_port_spec.rb
+++ spec/out_port_spec.rb
@@ -6,24 +6,24 @@ describe OutPort do
 
   before(:each) do 
     @midi_out = double 
+    @port = OutPort.new(@midi_out)
   end
 
   it 'sends a midi note-on event' do
     expect(@midi_out).to receive(:puts).with(OutPort::MIDI_ON, 40, 100)
-    OutPort.new(@midi_out).send_on(40, 100)
+    @port.send_on(40, 100)
   end
 
   it 'sends a midi note-off event' do
     expect(@midi_out).to receive(:puts).with(OutPort::MIDI_OFF, 40, 100)
-    OutPort.new(@midi_out).send_off(40, 100)
+    @port.send_off(40, 100)
   end
 
   it 'clears all playing notes' do
     expect(@midi_out).to receive(:puts).with(OutPort::MIDI_ON, 40, 100)
     expect(@midi_out).to receive(:puts).with(OutPort::MIDI_OFF, 40, 100)
-    out = OutPort.new(@midi_out)
-    out.send_on(40,100)
-    out.clear
+    @port.send_on(40,100)
+    @port.clear
   end
 
 end
