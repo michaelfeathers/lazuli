e5690beb4fd577c2fdbfe7cb94f2774f20b84953
Michael Feathers <michael.feathers@gmail.com>
2015-12-04 17:03:03 -0500

    refactored pod enumerator

--- lib/pod.rb
+++ lib/pod.rb
@@ -1,4 +1,11 @@
 
 
 class Pod
+  def each
+    return enum_for(:each) unless block_given?
+
+    loop do
+      yield EmptyCommand.new
+    end
+  end
 end
--- spec/pod_spec.rb
+++ spec/pod_spec.rb
@@ -8,15 +8,11 @@ end
 describe Pod do
 
   before(:each) do 
-    @pod = Enumerator.new do |e|
-     loop do 
-        e << EmptyCommand.new
-      end
-    end
+    @pod_stream = Pod.new.each
   end
 
   it 'returns an empty command on an empty pattern' do
-    expect(@pod.take(3).map(&:class).map(&:to_s)).to eq(['EmptyCommand'] * 3)
+    expect(@pod_stream.take(3).map(&:class).map(&:to_s)).to eq(['EmptyCommand'] * 3)
   end
 
 end
